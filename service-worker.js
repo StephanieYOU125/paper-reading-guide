73a1dca4eb7ffa68d5362ea0c9cfa4c1c60baedd
Wun-Sin You <152066727+StephanieYOU125@users.noreply.github.com>
2026-09-15 17:50:19 +0800

    Refresh PWA cache for usage guide

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='paper-compass-v9';
+const CACHE='paper-compass-v10';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
